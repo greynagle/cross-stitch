dd505eb00c7a431d463a43d5b78ab83ae077f7f2
Grey Nagle <greynagle@gmail.com>
2020-12-15 19:39:14 -0500

    learning through errors how to deal with images

--- src/Test.java
+++ src/Test.java
@@ -1,30 +1,40 @@
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Test extends JPanel {
 
     public void paint(Graphics g) {
-        Image img = createImageWithText();
+        Image img = null;
+        try {
+            img = combiningImages();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         g.drawImage(img, 0,0,this);
     }
 
-    private Image createImageWithText() {
-        BufferedImage bufferedImage = new BufferedImage(200,200,BufferedImage.TYPE_INT_RGB);
-        Graphics g = bufferedImage.getGraphics();
+    private Image combiningImages() throws IOException {
+//        BufferedImage bufferedImage = new BufferedImage(200,200,BufferedImage.TYPE_INT_RGB);
 
-        g.drawString("www.tutorialspoint.com", 20,20);
-        g.drawString("www.tutorialspoint.com", 20,40);
-        g.drawString("www.tutorialspoint.com", 20,60);
-        g.drawString("www.tutorialspoint.com", 20,80);
-        g.drawString("www.tutorialspoint.com", 20,100);
+        File path = new File("./images/");
 
-        return bufferedImage;
+        // load source images
+        BufferedImage image = ImageIO.read(new File(path, "image.png"));
+        BufferedImage overlay = ImageIO.read(new File(path, "overlay.png"));
+
+// create the new image, canvas size is the max. of both image sizes
+        int w = Math.max(image.getWidth(), overlay.getWidth());
+        int h = Math.max(image.getHeight(), overlay.getHeight());
+
+        return new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
     }
-
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.getContentPane().add(new Test());
